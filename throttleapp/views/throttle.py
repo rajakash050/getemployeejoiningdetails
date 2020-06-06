b9bb144c5aa95f6bc0c17eebd8c4c11aac19148d
akash raj <rajaakash1994@gmail.com>
2020-06-07 00:48:23 +0530

    added comments

--- throttleapp/views/throttle.py
+++ throttleapp/views/throttle.py
@@ -1,12 +1,15 @@
 from awaazapp.models.getmembers import *
 from awaazapp.common.db import getquery
-from django.http import Http404,HttpResponse, HttpResponseBadRequest,HttpRequest
+from django.http import Http404,HttpResponse, HttpResponseBadRequest,HttpRequest, JsonResponse
 import json
 
 import projectname.settings as settings
 domain_url = settings.SITE_URL
 
 def getMemberDetails(limit=None):
+    """"
+    get member details
+    """
     if limit == None:
         limit = 100
     get_article = "select * from member LIMIT {}".format(limit)
@@ -16,25 +19,24 @@ def getMemberDetails(limit=None):
 def getMmemberActivity(memberid):
     get_activity = "SELECT * from activity_periods WHERE memberid={}".format(memberid)
     response = getquery(get_activity)
-    # print("hhhhhhhhhhhhhhhhhhhhhhhhhhhh")
     activity_res = []
     for i in response:
-        # i['start_date'] = str(i['start_date'].strftime('%m/%d/%Y'))
-        # i['end_date'] = str(i['end_date'].strftime('%m/%d/%Y'))
         temp_res = {'start_date': str(i['start_date'].strftime('%m/%d/%Y')),
                         'end_date': str(i['end_date'].strftime('%m/%d/%Y'))}
         activity_res.append(temp_res)
-        # print(i['start_date'])
-    # print("iiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+
     return activity_res
 
+
 def iterate(lst, start, end , final_resp):
+    """"
+    function to iterate over each member data and get activity periods of each
+    """
     if start < 0 or end >= len(lst) or start > end:
         return final_resp
     # print(lst[start])
 
     activity_resp = getMmemberActivity(lst[start]['memberid'])
-    temp_resp = {}
     temp_resp = {"id":lst[start]['memberid'],"member_name":lst[start]['member_name'],"member_desc":lst[start]['member_desc']}
     temp_resp['activity_periods'] = activity_resp
 
@@ -45,8 +47,10 @@ def iterate(lst, start, end , final_resp):
     return final_resp
 
 
-
 def get_all_member(requests):
+    """"
+    main function request to get and merge all member details in one json
+    """
     get_member_details = getMemberDetails()
 
     # print(get_member_details)
@@ -54,6 +58,5 @@ def get_all_member(requests):
     final_resp = {"ok":True,'members':[]}
     final_resp = iterate(get_member_details, 0, (len(get_member_details) - 1), final_resp)
     print(json.dumps(final_resp))
-    bb = json.dumps(final_resp)
-    return HttpResponse(bb)
+    return JsonResponse(final_resp)
 
